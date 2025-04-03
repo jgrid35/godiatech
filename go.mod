1a2fb55d9172445ed9aaaab3f9e3a6def3b61ff7
jgrid35 <jgirard44@proton.me>
2025-04-03 15:59:36 +0200

    samba script

---
+++ go.mod
@@ -0,0 +1,11 @@
+module mediatech
+
+go 1.24.1
+
+require (
+	github.com/geoffgarside/ber v1.1.0 // indirect
+	github.com/hirochachacha/go-smb2 v1.1.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/secsy/goftp v0.0.0-20200609142545-aa2de14babf4 // indirect
+	golang.org/x/crypto v0.0.0-20200728195943-123391ffb6de // indirect
+)
---
+++ samba.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"fmt"
+	iofs "io/fs"
+	"net"
+	"regexp"
+
+	"github.com/hirochachacha/go-smb2"
+	"github.com/joho/godotenv"
+)
+
+type MovieFile struct {
+	file     string
+	srtFile  string
+	imdbFile string
+}
+
+func main() {
+	movieChan := make(chan MovieFile)
+	quit := make(chan int)
+	var movies []MovieFile
+	go readSambaFiles(movieChan, quit)
+	for {
+		select {
+		case movie := <-movieChan:
+			movies = append(movies, movie)
+		case <-quit:
+			fmt.Println(movies)
+			fmt.Println("Quitting...")
+			return
+		}
+	}
+}
+
+func readSambaFiles(c chan MovieFile, quit chan int) {
+	var envs map[string]string
+	envs, err := godotenv.Read(".env")
+
+	freeboxUser := envs["FREEBOX_USERNAME"]
+	freeboxPassword := envs["FREEBOX_PASSWORD"]
+	freeboxIP := envs["FREEBOX_IP"]
+	freeboxSmbPort := envs["FREEBOX_SMB_PORT"]
+	freeboxMediaFolder := envs["FREEBOX_MEDIA_FOLDER"]
+
+	conn, err := net.Dial("tcp", fmt.Sprintf("%v:%v", freeboxIP, freeboxSmbPort))
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+
+	d := &smb2.Dialer{
+		Initiator: &smb2.NTLMInitiator{
+			User:     freeboxUser,
+			Password: freeboxPassword,
+		},
+	}
+
+	s, err := d.Dial(conn)
+	if err != nil {
+		panic(err)
+	}
+	defer s.Logoff()
+
+	fs, err := s.Mount("Freebox")
+	if err != nil {
+		panic(err)
+	}
+	defer fs.Umount()
+
+	matches, err := iofs.Glob(fs.DirFS(freeboxMediaFolder), "*")
+	if err != nil {
+		panic(err)
+	}
+
+	imdbFileRegexp := "^tt[0-9]*"
+	fileNameRegexp := "(\\.mkv|\\.avi|\\.mp4)$"
+	srtFileNameRegexp := "(\\.srt)$"
+
+	for _, match := range matches {
+		matches, _ := iofs.Glob(fs.DirFS(fmt.Sprintf("%v/%v", freeboxMediaFolder, match)), "*")
+		var movie MovieFile
+
+		for _, match := range matches {
+			imdbFileMatch, _ := regexp.MatchString(imdbFileRegexp, match)
+			if imdbFileMatch {
+				movie.imdbFile = match
+			}
+
+			fileMatch, _ := regexp.MatchString(fileNameRegexp, match)
+			if fileMatch {
+				movie.file = match
+			}
+
+			srtFileMatch, _ := regexp.MatchString(srtFileNameRegexp, match)
+			if srtFileMatch {
+				movie.srtFile = match
+			}
+		}
+
+		c <- movie
+	}
+	fmt.Println("No more files to read")
+	quit <- 0
+	fmt.Println("Exiting readSmbFiles")
+}
